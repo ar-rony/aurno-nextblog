55b692a2983fcbadf30938760342bbf0470511b6
ar-rony <83110883+ar-rony@users.noreply.github.com>
2024-09-01 23:05:10 +0600

    Featured Post Section Completed

--- src/components/CatButton.jsx
+++ src/components/CatButton.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
  
 const CatButton = ({url, text}) => {
   return (
-    <Link className='px-6 py-2 text-light ring-2 ring-light rounded-full bg-dark w-max hover:scale-105 transition-all ease duration-100' href={url}>{text}</Link>
+    <Link className='px-6 py-2 capitalize text-light ring-2 ring-light rounded-full bg-dark w-max hover:scale-105 transition-all ease duration-100' href={url}>{text}</Link>
   )
 }
 
