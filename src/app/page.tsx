8115fb97977805c326fe0f8de9197fcd103545f5
ar-rony <83110883+ar-rony@users.noreply.github.com>
2024-09-01 19:24:56 +0600

    Header Design Completed and Banner Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ import Image from 'next/image'
 const Home = () => {
   return (
     <main className="">      
+      {/* Main Blog Banner */}
+      <div className="bg-dark/60 bg-cover bg-center h-96 md:h-[500px] p-5 md:px-14 md:py-20 flex flex-col justify-end rounded-lg gap-6 *:text-light relative">
+        <Image src={"/blogs/homepagebanner.jpg"} alt='' fill className='object-cover rounded-lg -z-10'/>
+        <CatButton url={"/"} text={"Web Development"}/>
+        <h1 className='w-full lg:w-8/12 text-xl sm:text-2xl md:text-3xl lg:text-4xl'>Building Progressive Web Apps: Bridging the Gap Between Web and Mobile</h1>
+        <p className='hidden text-xl md:block w-full lg:w-9/12'>Integrating mindfulness practices helps developers cultivate present-moment awareness, fostering focus, problem-solving, and work-life balance.</p>
+      </div>
       
     </main>
   )
