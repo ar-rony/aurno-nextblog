457386f103d4d1dc6b27c2a22a1afe1ed0adfaab
ar-rony <83110883+ar-rony@users.noreply.github.com>
2024-10-05 22:26:45 +0600

    Node_Modoules updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { posts } from "#site/content";
 import FeaturedPosts from "@/components/Home/FeaturedPosts";
 import HomeCoverSection from "@/components/Home/HomeCoverSection";
 import RecentPosts from "@/components/Home/RecentPosts";
-import Image from "next/image";
 
 export default function Home() {
   return (
